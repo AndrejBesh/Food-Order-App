d5af24b6f5d5c080779fa08b571ba8e2b95eb992
AndrejBesh <beslievski.a@gmail.com>
2022-05-31 09:00:40 +0200

    Added Components

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,27 @@
+import { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from './components/Meals/Meals.js'
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/Cart-Provider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
